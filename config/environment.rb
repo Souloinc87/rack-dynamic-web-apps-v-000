1d5638130e21920abcda7d78eb85d7c583dbf034
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-24 07:14:07 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,22 +3,3 @@ Bundler.require
 
 require_relative '../app/application'
 
-class Application
-  def call
-    resp = Rack::Response.new
-
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
-
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
-    if num_1 == num_2 and num_2 == num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
-  end
-end
