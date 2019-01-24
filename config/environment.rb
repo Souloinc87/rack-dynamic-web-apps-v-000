b1fa846b05d5c6d34f5dd8348d1e8bb7423c94f4
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-24 07:11:38 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,19 @@ require 'bundler/setup'
 Bundler.require
 
 require_relative '../app/application'
+
+class Application
+  def call
+    resp = Rack::Response.new
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    if num_1 == num_2 and num_2 == num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end 
+  end
+end
